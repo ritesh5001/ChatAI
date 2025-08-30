0e6a4f76e1bb111c3a7908bed9c1081aed3600e6
Ritesh5001 <rgiri5001@gmail.com>
2025-08-31 01:08:25 +0530

    socket middleware implemented

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,7 +1,34 @@
 const { Server } = require("socket.io");
+const cookie = require('cookie');
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
 
  function initSocketServer(httpServer){
     const io = new Server(httpServer,{})
+
+    io.use(async(socket,next)=>{
+
+        const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+        if(!cookies.token){
+            next(new Error(" Authentication error: no token found"))
+        }
+
+        try {
+
+            const decoded = jwt.verify(cookies.token,process.env.JWT_SECRET);
+
+            const user = await userModel.findById(decoded.id);
+
+            socket.user = user;
+             
+            next();
+
+        } catch (err) {
+            next (new Error("Authentication error: Invalid token"));
+        }
+
+    })
 
     io.on("connection",(socket)=>{
         console.log("New Socket connection", socket.id);
