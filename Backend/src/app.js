643b409ad8e537ff5855ff6cd0b89e9e9b01deb5
Ritesh5001 <rgiri5001@gmail.com>
2025-09-05 03:02:02 +0530

    frontend register and login complete

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const cookieParser = require('cookie-parser')
-
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 /*Routes*/
 const authRoutes = require('./routes/auth.routes.js')
 const chatRoutes = require('./routes/chat.routes.js');
 
 /* Using middlewares */
 const app = express();
+
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
+
 import './App.css'
+import  AppRoutes  from "./AppRoutes";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const setSystemTheme = () => {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      document.body.setAttribute('data-theme', prefersDark ? 'dark' : 'light');
+    };
+    setSystemTheme();
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setSystemTheme);
+    return () => {
+      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', setSystemTheme);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppRoutes />
     </>
-  )
+  );
 }
 
 export default App
